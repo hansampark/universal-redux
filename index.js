a12ba719a638b555b137d1fff6440a33991b4e43
Arthur Park <arthur.park@tridonic.com>
2016-07-18 08:07:07 -0700

    Clean up index

--- index.js
+++ index.js
@@ -3,16 +3,12 @@
 // Register babel to have ES6 support on the server
 require('babel-polyfill');
 require('babel-register');
-var hook = require('css-modules-require-hook');
+global.Promise = require('bluebird');
+const hook = require('css-modules-require-hook');
 
 hook({
   extensions: ['.css', '.scss']
 });
-global.Promise = require('bluebird');
 
-// // Require the intl polyfill
-// require("./src/server/intl-polyfill");
-
-// Prevent issues with libraries using this var (see http://tinyurl.com/pcockwk)
 delete process.env.BROWSER;
 const server = require('./app/server');
