a70eff09f1a12ad3bd9494134ed9205d9fa3321f
Hansam Park <hansolo@Hansolos-MBP.attlocal.net>
2016-08-15 14:59:19 -0700

    added material ui

--- app/main.jsx
+++ app/main.jsx
@@ -3,11 +3,15 @@ import { render } from 'react-dom';
 import { persistStore } from 'redux-persist';
 import localforage from 'localforage';
 import { AppContainer } from 'react-hot-loader';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import configureStore from './store/configure-store';
 import Root from './root';
 
 const rootEl = document.getElementById('app');
 const store = configureStore();
+// Needed for onTouchTap in MaterialUI
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const onRehydrate = () => {
   render(
--- app/root.jsx
+++ app/root.jsx
@@ -3,11 +3,14 @@
  * @flow
  */
 import React from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { Router, browserHistory } from 'react-router';
 import { routes } from './routes';
 
 const Root = () => (
-  <Router routes={routes} history={browserHistory} />
+  <MuiThemeProvider>
+    <Router routes={routes} history={browserHistory} />
+  </MuiThemeProvider>
 );
 
 export default Root;
